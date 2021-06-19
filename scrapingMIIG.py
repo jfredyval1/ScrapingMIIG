059d6e89e22259c11d90f4409ce97e5f1d022df0
jfredyval1 <jfredyval1@gmail.com>
2021-06-19 09:14:22 -0500

    Actualización, creación de tabla para primeros resultados de búsqueda

--- scrapingMIIG.py
+++ scrapingMIIG.py
@@ -39,9 +39,27 @@ WebDriverWait(driver, 5)\
     .until(EC.element_to_be_clickable((By.XPATH,
                                       '//*[@id="ctl00_ctl51_g_1b55f9d1_80ba_407b_ab58_82eed386edb2_ctl00_wpCajaBusqueda_csr_sbox"]')))\
     .send_keys('Hidrogeología')
+time.sleep(1)
 #Enviar busqueda
 WebDriverWait(driver, 5)\
     .until(EC.element_to_be_clickable((By.XPATH,
                                       '//*[@id="ctl00_ctl51_g_1b55f9d1_80ba_407b_ab58_82eed386edb2_ctl00_wpCajaBusqueda_csr_SearchLink"]')))\
     .click()
+# Capturar infomración de texto
+WebDriverWait(driver, 5)\
+    .until(EC.element_to_be_clickable((By.XPATH,
+                                      '//*[@id="ctl00_ctl51_g_6459c858_7354_4a84_9992_505f0b4f1814_csr1_groupContent"]')))
 
+texto_busqueda=driver.find_element_by_xpath('//*[@id="ctl00_ctl51_g_6459c858_7354_4a84_9992_505f0b4f1814_csr1_groupContent"]')
+texto_busqueda=texto_busqueda.text
+# Creación de un dataframe
+texto_col = texto_busqueda.split('\n')
+# Crear listas vacias para almacenar información
+titulo =list()
+fecha=list()
+# Almacenar la información como una tabla de dos variables
+for i in range(0,len(texto_col),3):    #Salto de 3 dado que las variables
+    titulo.append(texto_col[i])        #organizan de la forma:
+    fecha.append(texto_col[i+1])       #Títutlo, Fecha, Texto con hipervinculo ver elemento
+df = pd.DataFrame({'Título':titulo,'Fecha':fecha,'Buesqueda por:':'Hidrogeología'})
+print(df)
